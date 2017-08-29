597bbc5959a5e38eabbb97a22eb691b5688d41b3
neeraj-lad <neeraj.lad@yandex.com>
2017-08-28 23:29:47 -0400

    Add ms3

---
+++ ms3/ms3.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Source:
+ * https://coolestguidesontheplanet.com/how-to-connect-to-a-mysql-database-with-php/
+ * Microservice3 (ms3.php) listens on port 9080. It connects to the MySQL database.
+ *
+ */
+
+$id = $_GET['id'];
+
+$servername = '127.0.0.1';
+$username = 'root';
+$password = 'admin';
+$dbname = 'sga-neeraj-lad-asgn1';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connection_error)	die('Connection Failed: '.$conn->connect_error);
+
+$sql = "SELECT * FROM `Movies` WHERE `Rank` = ".$id;
+$res = $conn->query($sql) or die(mysql_error());
+
+$result = "";
+while ($row = $res->fetch_assoc()) {
+	foreach($row as $key => $value) {
+		$result .= $key;
+		$result .= ":";
+		$result .= $value;
+		$result .= ", ";	
+	}
+}
+echo $result;
+?>
