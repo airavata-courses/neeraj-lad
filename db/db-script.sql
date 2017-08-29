4ae7b9cff63dae2bc03ca38fff4a8fc61192bbfb
neeraj-lad <neeraj.lad@yandex.com>
2017-08-28 23:30:56 -0400

    Add db script

---
+++ db/db-script.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS `sga-neeraj-lad-asgn1`;
+USE `sga-neeraj-lad-asgn1`;
+
+CREATE TABLE IF NOT EXISTS `Movies`(Rank int, Rating int, Name varchar(100), NoOfReviews int);
+
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (1, 99, 'The Wizard of Oz (1939)', 109);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (2, 100, 'The Third Man (1949)', 76);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (3, 100, 'Citizen Kane (1941)', 73);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (4, 100, 'Mad Max: Fury Road (2015)', 357);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (5, 100, 'Das Cabinet des Dr. Caligari (1920)', 48);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (6, 100, 'All About Eve (1950)', 64);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (7, 99, 'The Godfather (1972)', 84);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (8, 98, 'Inside Out (2015)', 318);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (9, 100, 'Modern Times (1936)', 53);
+INSERT INTO `Movies` (Rank, Rating, Name, NoOfReviews) VALUES (10, 99, 'Metropolis (1927)', 115);
