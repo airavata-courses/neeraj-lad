1d7e04557ae13a4952972240bd702c012afbed71
neeraj-lad <neeraj.lad@yandex.com>
2017-09-11 23:28:45 -0400

    Add code for RabbitMQ sender and receiver in gateway.js

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -7,33 +7,61 @@
 var restify = require('restify');
 var request = require('request');
 var url = require('url');
+var amqp = require('amqplib/callback_api');
+
+var ms1Key = '#.ms1.#';
+var ms2Key = '#.ms2.#';
+var ms3Key = '#.ms3.#';
 
 var server = restify.createServer();
 server.listen(7080, function(){
 	console.log('%s listening at %s', server.name, server.url);
 });
-server.get('/?id=:id', respond);
+
 server.get('/:id', respond);
 
 function respond(req, res, next){
+	var id = req.params.id;
+
+	send(ms1Key, id, function(error, ms1ResID) {
+		send(ms2Key, ms1ResID, function(error, ms2ResID) {
+			send(ms3Key, ms2ResID, function(error, DBRow) {
+				//call to respond
+			});
+		});
+	});
 	res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	res.setHeader('content-type', 'application/json');
 	res.writeHead(200);
+	//res.write -> DBRow
+	res.end();
+	return next();
+}
 
-	/*
-	var par = url.parse(req.url, true).query;
-	var id = par.id;
-	*/
-	var id = req.params.id;
-	var ms1 = 'http://localhost';
-	var port = 8080;
-	var path = id;
-	var myurl = ms1 + ':' + port + '/' + path;
-
-	request(myurl, function(error, response, body){
-		res.write(body);
-		res.end();
-		return next();
+function send(key, msg) {
+	amqp.connect('amqp://localhost', function(err, send_conn) {
+	  send_conn.createChannel(function(err, send_ch) {
+		var ex = 'gateway_exchange';
+		send_ch.assertExchange(ex, 'topic', {durable: false});
+		send_ch.publish(ex, key, new Buffer(msg));
+		console.log(" [x] Sent %s:'%s'", key, msg);
+	  });
+	  setTimeout(function() { send_conn.close(); process.exit(0) }, 500);
 	});
 }
+
+amqp.connect('amqp://localhost', function(err, conn) {
+	conn.createChannel(function(err, ch) {
+		var ex = 'gateway_exchange';
+		ch.assertExchange(ex, 'topic', {durable: false});
+		ch.assertQueue('', {exclusive: true}, function(err, q) {
+			console.log(' [*] Waiting for messages. To exit press CTRL + C');
+			var key = '#.gw.#';
+			ch.bindQueue(q.queue, ex, key);
+			ch.consume(q.queue, function(message) {
+				console.log(' [x] Received %s: %s', message.fields.routingKey, message.content.toString());
+			}, {noAck: true});
+		});
+	});
+});
